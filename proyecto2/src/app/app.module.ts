5687a5cdd0904d7f8213ad37dac91c1779cec04b
SilFernandez <102128832+SilFernandez@users.noreply.github.com>
2023-05-16 19:34:14 -0300

    Components

--- proyecto2/src/app/app.module.ts
+++ proyecto2/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoapComponent } from './components/logoap/logoap.component';
+import { RedessocComponent } from './components/redessoc/redessoc.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoapComponent,
+    RedessocComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
---
+++ proyecto2/src/app/components/redessoc/redessoc.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-redessoc',
+  templateUrl: './redessoc.component.html',
+  styleUrls: ['./redessoc.component.css']
+})
+export class RedessocComponent {
+
+}
